8f08465697aa36bea785f0b09e14b18baf122100
quistxnd <quistandd@gmail.com>
2026-05-13 02:14:29 +0700

    Update Main.java

--- lab04/FinanceTracker/src/main/java/org/example/Main.java
+++ lab04/FinanceTracker/src/main/java/org/example/Main.java
@@ -7,10 +7,10 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        // 1. Инициализируем базу данных (создаст таблицы, если их нет)
+       
         DatabaseManager.getInstance();
 
-        // 2. Запускаем графический интерфейс в специальном потоке для UI
+        
         SwingUtilities.invokeLater(() -> {
             FinanceTrackerGUI gui = new FinanceTrackerGUI();
             gui.setVisible(true);
